a811919e1bad0b436493940e2a75494f06e241f3
stephenwhitted <stephent.whitted@gmail.com>
2024-06-21 16:12:00 -0400

    commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TodoInput.css;'
+
+const TodoInput = ({ newTodo, setNewTodo, handleAddTodo }) => {
+  return (
+    <div className="todo-input">
+    <input
+      type="text"
+      value={newTodo}
+      onChange={(e)} => setNewTodo(e.target.value)}
+      placeholder="Add task"
+    />
+    <button onClick={handleAddTodo}>Add</button>
+  </div>
+  );
+};
+
+export default TodoInput;
